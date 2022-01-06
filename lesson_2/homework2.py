f61e2a8653f25e4ad9452ed87a0cb32f7a984b0c
IGorbunov <simocode@mail.ru>
2022-01-06 14:45:28 +0200

    решение задачи

--- lesson_2/homework2.py
+++ lesson_2/homework2.py
@@ -1,3 +1,7 @@
 msg = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-if isinstance(msg.index, int):
-print(msg)
+#поиск по всем элементам списка либо число либо значение +5
+for index in range(len(msg)):
+    if msg[index].isdigit() or msg[index] == '+5':
+        msg[index] = f'"0{msg[index]}"'
+msg_string = " ".join(msg)
+print(msg_string)
